ecd173ae452d012e8e78d8dbc8c6e2a1715511e0
Greenpatch <tishaka74@gmail.com>
2026-03-06 10:18:29 +0000

    update ; a propos

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,21 +8,7 @@ const About = () => {
       <Logo />
       <Navigation />
       <h1>A propos</h1>
-      <br />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-        quia culpa beatae deleniti dignissimos veniam debitis veritatis
-        temporibus, repellendus nihil ea omnis cum hic praesentium quisquam ad
-        fugiat! Labore ipsa perspiciatis, aut esse odio repellendus distinctio
-        doloremque sint provident doloribus blanditiis, quaerat rerum. Facere
-        harum in ratione eos, minima blanditiis dicta ad minus quos temporibus
-        adipisci excepturi ab exercitationem iure esse enim, natus repellat
-        error reiciendis iusto. Dicta quibusdam, ipsum enim doloribus veritatis
-        officia voluptas, similique in veniam sed possimus? Magnam voluptates
-        reiciendis, non ipsum, fugiat minus quasi architecto deleniti voluptate
-        quis officia corporis natus veniam dicta excepturi atque commodi?
-      </p>
-      <br />
+
       <p>
         À propos
 Infospays est une application web interactive permettant de découvrir les pays du monde entier en un coup d'œil.
